572d2c0932d26f3cee4eefbc4b66e483af63aabf
Dustin Hurtt <dustin_hurtt@outlook.com>
2024-05-10 14:22:12 -0400

    car is showing up on screen

---
+++ js/player.js
@@ -0,0 +1,25 @@
+class Player {
+
+    constructor(gameScreen, left, top, width, height, imgSrc) {
+        this.gameScreen = gameScreen
+        this.left = left
+        this.top = top
+        this.width = width
+        this.height = height
+        this.directionX = 0
+        this.directionY = 0
+        this.element = document.createElement('img')
+        this.element.src = imgSrc
+        this.element.style.position = 'absolute'
+        this.element.style.left = `${this.left}px`
+        this.element.style.top = `${this.top}px`
+        this.element.style.height = `${this.height}px`
+        this.element.style.width = `${this.width}px`
+
+        this.gameScreen.appendChild(this.element)
+    }
+
+
+}
+
+
